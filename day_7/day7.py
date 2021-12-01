93d9d507b62cfaf6a1414189a4378b2d8b58cd5b
Jade Cohen <Jade@Jades-MacBook-Pro.local>
2021-12-01 23:21:28 +0200

    same letter guessed correttly

--- day_7/day7.py
+++ day_7/day7.py
@@ -12,16 +12,23 @@ print(yourAnswer)
 end_of_game = False
 
 lives = len(chosen_word)
+total_lives = len(chosen_word)
+
 
 while not end_of_game:
     guess = input("guess a letter: ").lower()
+    if guess in yourAnswer:
+        print("you already guessed this letter:", guess)
     for position in range(len(chosen_word)):
         letter = chosen_word[position]
         if letter == guess:
             yourAnswer[position] = letter
+            print("correct!")
     if guess not in chosen_word:
+        print("your guess:", guess)
         lives -= 1
-        print(lives)
+        print("Incorrect, please try again!")
+        print("Your total lives left:", lives, "/", total_lives)
     if lives == 0:
         end_of_game = True
         print("you lose")
